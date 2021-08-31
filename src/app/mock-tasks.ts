fedfa70daea6e56f431021f70b357d5c968684ea
fabiolorenzi <fabiolorenzi@gmail.com>
2021-08-31 21:47:03 +0100

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task";
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Doctor appointment",
+        day: "tomorrow",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Date",
+        day: "yesterday",
+        reminder: false
+    },
+    {
+        id: 3,
+        text: "IDK",
+        day: "3th May",
+        reminder: true
+    }
+];
